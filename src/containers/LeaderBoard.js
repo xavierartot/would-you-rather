0e011c301b2a4ad823500e2a7cb0000e8369f5ee
xav <xavierartot@gmail.com>
2018-08-23 18:42:30 -0500

    feature: leader board logic

--- src/containers/LeaderBoard.js
+++ src/containers/LeaderBoard.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
+import { Table } from 'reactstrap'
+import { connect } from 'react-redux'
+import LeaderBoardList from './LeaderBoardList'
 
 class LeaderBoard extends Component {
   render() {
+    const { users, totalQuestion } = this.props
+
+    const arrayUsers = Object.values(users)
+    // console.log(totalQuestion.length)
     return (
       <div className="LeaderBoard">
-        LeaderBoard
+        <Table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>User</th>
+              <th>Poll Created</th>
+              <th>Poll Unanswered</th>
+            </tr>
+          </thead>
+          <tbody>
+            {arrayUsers.map((user, i) => (
+              <tr key={user.id}>
+                <th scope={user.id}>{i + 1}</th>
+                <th>{user.name}</th>
+                <th>{user.questions && user.questions.length}</th>
+                <th>
+                  <LeaderBoardList list={user.answers} totalQuestion={totalQuestion} />
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     )
   }
 }
-export default LeaderBoard
+function mapStateToProps({ users, questions }) {
+  const totalQuestion = Object.values(questions)
+  // totalQuestion = totalQuestion.length
+  console.log(totalQuestion)
+  return {
+    users,
+    questions,
+    totalQuestion,
+  }
+}
+export default connect(mapStateToProps)(LeaderBoard)
---
+++ src/containers/LeaderBoardList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class LeaderBoardList extends Component {
+  render() {
+    const { list, totalQuestion } = this.props
+    const l = Object.keys(list)
+    console.log(totalQuestion.length)
+    return (
+      <div className="LeaderBoardList">
+        { totalQuestion.length - l.length }
+      </div>
+    )
+  }
+}
+export default LeaderBoardList
